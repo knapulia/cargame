55ac708fbbf154273fa1b80bf44a45012a9dc015
knapulia <knapulia@gmail.com>
2025-05-30 03:02:17 +0300

    Тести та flake8

--- cargame/tests/test_car_config.py
+++ cargame/tests/test_car_config.py
@@ -1,5 +1,6 @@
 from cargame.car_config import set_car_color_path, get_car_color_path
 
+
 def test_set_and_get_car_color_path():
     path = "assets/car_green.PNG"
     set_car_color_path(path)
--- cargame/tests/test_cloud.py
+++ cargame/tests/test_cloud.py
@@ -1,9 +1,11 @@
 import pygame
 from cargame.cloud import Cloud
 
+
 def test_cloud_reset_and_update(monkeypatch):
     pygame.init()
-    monkeypatch.setattr("pygame.image.load", lambda path: pygame.Surface((150, 50)))
+    monkeypatch.setattr("pygame.image.load",
+                        lambda path: pygame.Surface((150, 50)))
     cloud = Cloud(800, 600, "dummy.png")
 
     initial_x = cloud.x
